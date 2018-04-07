c20de1390aeae52d7b816896fcfc95f3851eb255
Chris Shafer <cshafer@skylinenet.net>
2018-04-07 12:18:23 -0400

    Code rework

--- LogSensorData.py
+++ LogSensorData.py
@@ -3,32 +3,7 @@ import Adafruit_DHT
 import time
 import sys
 from hx711 import HX711
-
-class hx:
-    scale = 0
-    def __init_(r,c):
-        scale = r*c
-
-
-class scales:
-
-class sensors:
-    hum = 70
-    temp = 80
-    scale1 = 10000
-    scale2 = 20000
-    #Get Grams
-    def get_grams():
-    #Get Oz
-    #Get Temp
-    #Get Humdity
-
-    #initalize Sensors
-    def __init__(self):
-
-
-
-
+import json
 
 def cleanAndExit():
     print "Cleaning..."
@@ -36,49 +11,39 @@ def cleanAndExit():
     print "Bye!"
     sys.exit()
 
-sensors = {
-    climSensor = {
-        sensor = Adafruit_DHT.DHT11,
-        humidity = 0;
-        temperature = 0;
-    },
-    scales = [
-        {
-            hx = HX711(20, 21),
-            oz = 0
-        },
-        {
-            hx = HX711(5, 6),
-            oz = 0
-        }
-    ]
-}
+def initializeHX(dout,pd_sck):
+    hxSensor = HX711(dout,pd_sck)
+    hxSensor.set_reference_unit = 406.773*35.274
+    hxSensor.set_reading_format("LSB", "MSB")
+    hxSensor.tare()
+    return hxSensor
 
-hx.set_reading_format("LSB", "MSB")
-hx2.set_reading_format("LSB", "MSB")
+def main():
+    hxSensors = [initializeHX(20,21), initializeHX(5,6)]
+    climSensor = Adafruit_DHT.DHT11
 
- 
-hx.reset()
-hx.tare()
-hx2.reset()
-hx2.tare()
+    while True:
+        weight = []
+        try:
+            humidity, temperature = Adafruit_DHT.read_retry(sensor, 4)
+            for hx in hxSensors:
+                hx.reset()
+                weight.append(hx.get_weight())
+            sensorResult = {
+                'urn1':  round(weight[0],0),
+                'urn2': round(weight[1],0),
+                'humidity': round(humidity,1),
+                'temperature': round(temperature,1)
+            }
+            #prints json
+            json.dumps(sensorResult)
 
-while True:
-    try:
-        humidity, temperature = Adafruit_DHT.read_retry(sensor, gpio)
-        val = hx.get_weight(5)
-        grams = val/406.773
-        oz=grams/35.274
-        oz=round(oz,0)
-        val2 = hx2.get_weight(5)
-        grams2 = val2/406.773
-        oz2=grams2/35.274
-        oz2=round(oz2,0)
-        print 'Urn 1=', oz, 'oz, Urn 2=', oz2, 'oz', ('{0:0.1f}, {1:0.1f}'.format(temperature, humidity))
-        hx.power_down()
-        hx2.power_down()
-        hx.power_up()
-        hx2.power_up()
-        time.sleep(5.0)
-    except (KeyboardInterrupt, SystemExit):
-        cleanAndExit()
+            #prints the same line with better formatting
+            print('Urn 1={:1.0f}pz, Urn 2={:1.0f}oz, {:0.1f}, {0.1f}'.format(weight[0],weight[1],temperature,humidity)
+            #print 'Urn 1=', oz, 'oz, Urn 2=', oz2, 'oz', ('{0:0.1f}, {1:0.1f}'.format(temperature, humidity))
+            time.sleep(5.0)
+        except (KeyboardInterrupt, SystemExit):
+            cleanAndExit()
+
+if __name__ == "__main__":
+    main()
--- test.py
+++
@@ -1,7 +0,0 @@
-class hx:
-    scale = 0
-    def __init__(self, r,c):
-        self.scale = r*c
-
-sensor = hx(10,10)
-print(sensor)
